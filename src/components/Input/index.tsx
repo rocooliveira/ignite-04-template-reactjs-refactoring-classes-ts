0767fe7c26b398f44f56e257b30a9e52f7fccfcb
Romulo Costa <Roco@MacBook-Pro-de-Edgar.local>
2022-02-28 15:16:10 -0300

    atividade completa

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,23 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
-
 import { useField } from '@unform/core';
-
+import React,{
+  InputHTMLAttributes,
+  useCallback, useEffect,
+  useRef,
+  useState
+} from 'react';
+import { IconBaseProps } from 'react-icons';
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+
+
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -1,30 +1,45 @@
-import { Component, createRef } from 'react';
+import {  useRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
 
-class ModalEditFood extends Component {
-  constructor(props) {
-    super(props);
+interface EditFood{
+  id: number,
+  name: string,
+  description: string,
+  price: number,
+  available: boolean,
+  image: string
+}
 
-    this.formRef = createRef()
-  }
+interface ModalEditFoodProps{
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleUpdateFood: (food: Omit<EditFood, 'id' | 'available'>) => void;
+  editingFood: EditFood;
+}
 
-  handleSubmit = async (data) => {
-    const { setIsOpen, handleUpdateFood } = this.props;
+const ModalEditFood = ({
+  isOpen, 
+  setIsOpen, 
+  handleUpdateFood,
+  editingFood
+}: ModalEditFoodProps) => {
+
+  const formRef =  useRef(null);
+
+  const handleSubmit = async (data: EditFood) => {
 
     handleUpdateFood(data);
     setIsOpen();
   };
 
-  render() {
-    const { isOpen, setIsOpen, editingFood } = this.props;
 
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit} initialData={editingFood}>
+        <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
           <h1>Editar Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -42,7 +57,7 @@ class ModalEditFood extends Component {
         </Form>
       </Modal>
     );
-  }
+
 };
 
 export default ModalEditFood;
